7490aa9871d33c3edc2675d60c177d00370d9b90
abeidahmed <abeidahmed92@gmail.com>
2021-08-04 11:45:13 +0530

    simplify error_serializer

--- backend/app/serializers/error_serializer.rb
+++ backend/app/serializers/error_serializer.rb
@@ -4,20 +4,15 @@ module ErrorSerializer
       return if errors.nil?
 
       json = {}
-      error_hash = if errors.is_a?(ActiveModel::Errors)
-                     errors.to_hash(true).map { |k, v| render_errors(errors: v, type: k) }
-                   else
-                     errors.to_hash.map { |k, v| render_errors(errors: v, type: k, klass: false) }
-                   end.flatten
+      json[:errors] = errors.to_hash.map { |key, val| render_errors(errors: val, type: key) }.flatten
 
-      json[:errors] = error_hash
       json
     end
 
-    def render_errors(errors:, type:, klass: true)
+    def render_errors(errors:, type:)
       errors.map do |msg|
         normalized_type = type.to_s.humanize
-        msg = "#{normalized_type} #{msg}" unless klass
+        msg = "#{normalized_type} #{msg}"
 
         { source: { pointer: "/data/attributes/#{type}" }, type: type, detail: msg }
       end
